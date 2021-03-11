f4d5310341f5a331ba6fc084d6afbd3920f29e85
Shayan Alinejad <carixo2805@gmail.com>
2021-03-11 13:09:01 +0100

    idk

--- main.js
+++ main.js
@@ -12,140 +12,6 @@ const mapHeight = 100;
 const mapWidth = 100;
 const mapSeed = Math.random();
 
-
-
-// class Point2D 
-// {
-//     /**
-//      * 
-//      * @param {int} x 
-//      * @param {int} y 
-//      */
-//     constructor(x, y)  
-//     {
-//         this.x = x;
-//         this.y = y;
-//     }
-// }
-
-
-
-// class Point3D 
-// {
-//     /**
-//      * 
-//      * @param {int} x 
-//      * @param {int} y 
-//      * @param {int} z 
-//      */
-//     constructor(x, y, z) 
-//     {
-//         this.x = x;
-//         this.y = y;
-//         this.z = z;
-//     }
-// }
-
-// class Hex 
-// {
-//     /**
-//      * 
-//      * @param {Point2D} point2D 
-//      */
-//     constructor(point2D) 
-//     {
-//         this.point2D = point2D;
-//         this.building = 0;
-//     }
-//     /**
-//      * 
-//      * @param {Point3D} point3D 
-//      */
-//     static cubeToAxial(point3D) 
-//     {
-//             return new Point2D(point3D.x, point3D.z);
-//     }
-//     /**
-//      * 
-//      * @param {Point2D} point2D 
-//      */
-//     static axialToCube(point2D)
-//     {
-//         let x = point2D.x;
-//         let z = point2D.y;
-//         let y = -x - z;
-//         return new Point3D(x, y, z);
-//     }
-
-//     /**
-//      * 
-//      * @param {Point3D} point3D 
-//      */
-//     static hexRound(point3D){
-//         let rx = Math.round(point3D.x)
-//         let ry = Math.round(point3D.y)
-//         let rz = Math.round(point3D.z)
-
-//         let x_diff = Math.abs(rx - point3D.x)
-//         let y_diff = Math.abs(ry - point3D.y)
-//         let z_diff = Math.abs(rz - point3D.z)
-
-//         if (x_diff > y_diff && x_diff > z_diff){
-//             rx = -ry-rz
-//         }
-//         else if (y_diff > z_diff){
-//             ry = -rx-rz
-//         }
-//         else{
-//             rz = -rx-ry
-//         }
-
-//         return new Point3D(rx, ry, rz);
-//     }
-
-
-//     /**
-//      * 
-//      * @param {Hex} hex 
-//      */
-//     static hexToPixel(hex) {
-//         let x = size * (3/2 * hex.point2D.x) + size + origo.x; // creating an x-basis vector from origin to the hexagon that is clicked, giving its central x- coordinate
-//         let y = size * (Math.sqrt(3)/2 * hex.point2D.y  +  Math.sqrt(3) * hex.point2D.y) + (size * Math.sin(Math.PI/3)) + origo.y; // creating an y-basis vector from origin to the hexagon that is clicked, giving its central y- coordinate
-//         return new Point2D(x, y);
-//     }
-
-
-//     /**
-//      * 
-//      * @param {Point2D} point2D 
-//      */
-//     static pixelToHex (point2D){
-//         let px = point2D.x - size - origo.x; // Changing the initial x coordinate to the middle of the top left hexagon
-//         let py = point2D.y - ((Math.sqrt(3) * size)/2) - origo.y; // Changing the initial y coordinate to the middle of the top left hexagon
-       
-//         console.log(`${origo.x}, ${origo.y}`)
-      
-//         let q = ( 2/3 * px) / size; // using the inverted function from hexToPixel function to get x from hex-coord
-//         let r = (-1/3 * px  +  Math.sqrt(3)/3 * py) / size; // using the inverted function from hexToPixel function to get y from hex-coord
-
-//         let point = this.cubeToAxial(this.hexRound(this.axialToCube(new Point2D(q, r))));
-
-//         point.y += Math.floor(point.x/2)
-       
-//         return point;
-//     }
-// }
-// class HUD
-// {
-//     buttonsList;
-//     infoLists;
-//     constructor(buttonsList, infoLists)
-//     {
-//         this.buttonsList = buttonsList;
-//         this.infoLists = infoLists;
-//     }
-// }
-
 class Button
 {
     pointStart;
@@ -225,16 +91,6 @@ function drawTile(tile, x, y)
     ctx.drawImage(hexSpritesheet, spriteX, spriteY, spriteWidth, spriteHeight, x, y, width, height);
 }
 
-// function loading() 
-// {
-//     loadedElements += 1;
-//     console.log(`${loadedElements}/${elementsToBeLoaded} loaded ` + Math.round(loadedElements/elementsToBeLoaded*100) + "%");
-//     if (loadedElements == elementsToBeLoaded) 
-//     {
-//         console.log(`${loadedElements}/${elementsToBeLoaded} loaded Game initializing`);
-//         init();
-//     }
-// }
 
 
 
@@ -420,14 +276,5 @@ function gameLoop() {
 
     requestAnimationFrame(gameLoop);
 }
-
-// var hexSpritesheet = new Image();
-// hexSpritesheet.src = "hexagonTerrain_sheet.png";
-// hexSpritesheet.onload = loading();
-
-// var HUDSprite = new Image();
-// HUDSprite.src = "hud.png";
-// HUDSprite.onload = loading();
-
 
 load();
