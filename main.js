bb25790644f8403d1ab1b0a3998ec396b34d8628
FlyingRainbowPotato <patrickfalkeid@gmail.com>
2021-03-19 09:33:32 +0100

    fps counter and minor changes

--- main.js
+++ main.js
@@ -13,7 +13,10 @@ const mapWidth = 100;
 const mapSeed = Math.random();
 const loadedWidth = (Math.ceil(canvas.width/((3/2)*size))+1);
 const loadedHeight = (Math.ceil(canvas.height/(size*(Math.sqrt(3))))+1);
+let lastCalledTime;
+let fps;
 ctx.lineWidth = 2;
+ctx.font = "30px Arial";
 
 
 // class Point2D 
@@ -271,7 +274,7 @@ function init()
  */
 
 function getBiome(coords) {
-    const freq = 0.1;
+    const freq = 0.15;
     noise.seed(mapSeed);
     const e = (noise.perlin2(freq * coords.x, freq * coords.y) + 1) / 2;
     // noise.seed(mapSeed / 2);
@@ -313,21 +316,21 @@ function drawGrid()
 {
     for 
     (
-        let x = Math.round(Hex.pixelToHex(new Point2D(-origo.x,-origo.y)).x/2); 
-        x < loadedWidth + Math.round(Hex.pixelToHex(new Point2D(-origo.x,-origo.y)).x/2); 
+        let x = Math.floor(Hex.pixelToHex(new Point2D(-origo.x,-origo.y)).x/2)-1; 
+        x < loadedWidth + Math.floor(Hex.pixelToHex(new Point2D(-origo.x,-origo.y)).x/2); 
         x++
     ) 
     {
         for 
         (
-            let y = Math.round(Hex.pixelToHex(new Point2D(-origo.x,-origo.y)).y/2);
-            y < loadedHeight + Math.round(Hex.pixelToHex(new Point2D(-origo.x,-origo.y)).y/2); 
+            let y = Math.floor(Hex.pixelToHex(new Point2D(-origo.x,-origo.y)).y/2)-1;
+            y < loadedHeight + Math.floor(Hex.pixelToHex(new Point2D(-origo.x,-origo.y)).y/2); 
             y++
         )
         {
             let hexCoord = Hex.hexToPixel(new Point2D(x,y));
             drawTile(getBiome(new Point2D(x,y)), new Point2D(x,y));
-            drawHexagon(hexCoord.x,hexCoord.y);
+            //drawHexagon(hexCoord.x,hexCoord.y);
         }
     }
 }
@@ -340,19 +343,19 @@ function keyHandlerDown(e)
 {
     if (e.key == "ArrowDown")
     {
-        scrollSpeed.y = -10;
+        scrollSpeed.y = -1;
     }
     else if (e.key == "ArrowUp")
     {
-        scrollSpeed.y = 10;
+        scrollSpeed.y = 1;
     }
     else if (e.key == "ArrowRight")
     {
-        scrollSpeed.x = -10;
+        scrollSpeed.x = -1;
     }
     else if (e.key == "ArrowLeft")
     {
-        scrollSpeed.x = 10;
+        scrollSpeed.x = 1;
     }
     else 
     {
@@ -415,11 +418,25 @@ document.onclick = mouseHandler;
 
 function gameLoop() {
     //Calculations
-    origo.add(scrollSpeed);
+    origo.add(scrollSpeed.normalize()*10);
+    
+    if(!lastCalledTime){
+        lastCalledTime = Date.now();
+        fps = 0;
+    }
+    delta = (Date.now() - lastCalledTime)/1000;
+    lastCalledTime = Date.now();
+    fps = 1/delta;
+
 
     //Animation
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     drawGrid();
+    ctx.fillStyle = "white";
+    ctx.fillRect(0,0,60,40);
+    ctx.fillStyle = "black";
+    ctx.fillText(Math.round(fps),10,30);
+
     //ctx.drawImage(HUDSprite, 0, 0, 1920, 1080, 0, 0, 1920, 1080);
 
     requestAnimationFrame(gameLoop);
