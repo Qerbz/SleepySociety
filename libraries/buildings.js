1492257840b65d971c3c4b0ddbf6ed324c2cd34f
Leizzz69 <leif.eggenfellner@gmail.com>
2021-04-09 10:13:28 +0200

    added and removed som classes and modified a little

--- libraries/buildings.js
+++ libraries/buildings.js
@@ -20,6 +20,18 @@ class Building {
         this.y = y;
         this.upgradeable = false;
         this.level = 0;
+        }
+    
+    /**
+     * 
+     * @param {Image} img The image of the building 
+     * @param {Number} inhabitants The maximum amount of allowed people living in the building
+     */
+
+    upgrade(img, inhabitants) {
+        this.level++;
+        this.img = img;
+        this.inhabitants = inhabitants;
     }
 
     /**
@@ -37,14 +49,12 @@ export class Housing extends Building {
 
     price;
     inhabitants;
-    level;
 
     /**
      * 
      * @param {Image} img The image of the house.
      * @param {Number} x The x-coordinate of the house.
      * @param {Number} y The y-coordinate of the house.
-     * @param {Number} price The price of building the building
      */
     
     constructor(img, x, y) {
@@ -53,41 +63,70 @@ export class Housing extends Building {
             work: 1,
             wood: 30
         };
-        this.inhabitants = inhabitants;
-        this.people = new Uint16Array(this.inhabitants);
-        this.level = 0;
-    }
-
-    upgrade(img, inhabitants) {
-        this.level++;
-        this.img = img;
-        this.inhabitants = inhabitants;
+        this.people = new Array();
+        this.inhabitants = 3;
     }
 }
 
+export class Commercial extends Building {
 
-class Workplace extends Building {
-    constructor(img, x, y, price, maxWorkers) {
+    price;
+    workers;
+
+    /**
+     * 
+     * @param {Image} img The image of the building
+     * @param {Number} x The x-coordinate of the building
+     * @param {Number} y The y-coordinate of the building
+     */
+
+    constructor(img, x, y) {
         super(img, x, y);
-        this.price = price;
-        this.maxWorkers = 
-        this.workers = Uint16Array(this.maxWorkers);
+        this.price = {
+            work: 3,
+            wood: 30
+        };
+        this.inhabitants = 0;
+        this.workers = new Array();
     }
 }
 
-export class Store extends Workplace {
+class Agriculture extends Building {
+
+    price;
 
     /**
      * 
      * @param {Image} img 
      * @param {Number} x 
      * @param {Number} y 
-     * @param {Number} price 
-     * @param {Number} workers 
+     * @param {Number} maxWorkers 
      */
 
-    constructor(img, x, y, price, workers) {
-        super(img, x, y, price, workers);
+    constructor(img, x, y, maxWorkers) {
+        super(img, x, y);
+        this.maxWorkers = maxWorkers;
+        this.price = {
+            work: 3,
+            wood: 40
+        }
+    }
+}
+
+class Electricity extends Building {
+
+    /**
+     * 
+     * @param {Image} img 
+     * @param {Number} x 
+     * @param {Number} y 
+     */
+
+    constructor(img, x, y, price, maxWorkers) {
+        super(img, x, y);
+        this.price = price;
+        this.maxWorkers = maxWorkers;
+        this.workers = new Array();
     }
 }
 
